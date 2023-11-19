9b78b7948e263af6095c95663f14dcf005478745
Pelao PC <codeferlu@gmail.com>
2023-11-19 10:39:53 -0300

    Archivos esenciales
    Subida de archivos

---
+++ index.js
@@ -0,0 +1,8 @@
+precio = 400000
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio
+
+var cantidad = Number(document.querySelector(".cantidad").innerText);
+var cantidadSpan = document.querySelector(".cantidad")
+var cantidadtotal = document.querySelector(".valor-total")
